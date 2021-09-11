1839d74c75ba8901b4680b440edd42c0727882ca
miles <miles1618@gmail.com>
2021-09-10 19:09:35 -0500

    randomized discovery tokens

--- cmd/harvest/primer.py
+++ cmd/harvest/primer.py
@@ -9,7 +9,7 @@ For each pair in the table:
 """
 import http
 import json
-
+from random import shuffle
 from internal import HC, DYNAMO
 from internal.service_sqs.sqs import ServiceSQS
 
@@ -19,10 +19,7 @@ SQS = ServiceSQS(HC.queue_harvest)
 def primer(event, context):
     # Scan the dynamo table for pairs and last_udpated
     # Create SQS Items for each of the pairs
-    all_tickers = DYNAMO.discovery_scan(HC.table_discovery)
-    if HC.env.upper() == "DEV" or HC.env.upper() == "TEST":
-        print(f"Slicing all tickers to reduce SAN API Usage. ENV: {HC.env}")
-        all_tickers = all_tickers
+    all_tickers = shuffle(DYNAMO.discovery_scan(HC.table_discovery))
     message_ids = [
         SQS.send_message(item.to_sqs_format(delay_seconds=min(delay * 3, 900)))
         for delay, item in enumerate(all_tickers)
